81ba2f2c329c46805f4d1e74aa5a23099f60c789
yusume <yusume@yusume-imac.local>
2019-01-29 22:35:56 +0300

    Initial Commit

---
+++ FLAPY BİRD(M.Y.A)Tests/FLAPY_BI_RD_M_Y_A_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FLAPY_BI_RD_M_Y_A_Tests.swift
+//  FLAPY BİRD(M.Y.A)Tests
+//
+//  Created by yusume on 29.01.2019.
+//  Copyright © 2019 yusume. All rights reserved.
+//
+
+import XCTest
+@testable import FLAPY_BI_RD_M_Y_A_
+
+class FLAPY_BI_RD_M_Y_A_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
